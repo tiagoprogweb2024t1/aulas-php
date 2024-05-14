fe7726e366058e82ea5480d571ffe0bc13d67164
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-14 17:26:08 -0300

    Introdução ao PHP

---
+++ 01-hello-world.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando com PHP</title>
+    <style>
+        p { color: red; }
+    </style>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+
+    <!-- Delimitadores de abertura/fechamento PHP -->
+    <?php
+    // Comando de saída: echo
+    echo "Olá!";
+    echo "<p style='text-align:center'>Senac Penha</p>";
+    echo '<p style="text-align:center">Senac Penha</p>';
+    echo "<p style=\"text-align:center\">Senac Penha</p>";
+    ?>
+    <!-- Delimitadores de abertura/fechamento PHP -->
+</body>
+</html>
