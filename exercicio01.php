38e6cff7770ddb02b76b47bb5be74af20406b6c1
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-15 15:18:54 -0300

    Correção com CSS e destaque

--- exercicio01.php
+++ exercicio01.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 01</title>
+    <style>
+        .um { color: red; }
+        .dois { color: purple; }
+    </style>
 </head>
 <body>
     <h1>Exercício 01</h1>
@@ -12,12 +16,19 @@
 <?php
 const NOME = "Chapolin";
 $idade = 25;
-
 echo "<p>".NOME." possui $idade anos.</p>";
 ?>
+<p> <b><?=NOME?></b>  possui <i><?=$idade?></i> anos.</p>
 
-<!-- Chapolin possui 25 anos." -->
-<p><?=NOME?> possui <?=$idade?> anos.</p>
+<p> 
+    <b class="um"><?=NOME?></b>  possui 
+    <i class="dois"><?=$idade?></i> anos.
+</p>
+
+<p> 
+    <span class="um"><?=NOME?></span>  possui 
+    <span class="dois"><?=$idade?></span> anos.
+</p>
 
 
 
