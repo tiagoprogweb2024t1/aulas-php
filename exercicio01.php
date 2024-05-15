997727ebb78c7d3b49bc80b356d18809d9083227
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-15 15:30:16 -0300

    Correção com Bootstrap

--- exercicio01.php
+++ exercicio01.php
@@ -4,12 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 01</title>
-    <style>
-        .um { color: red; }
-        .dois { color: purple; }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<div class="container">
     <h1>Exercício 01</h1>
     <hr>
 
@@ -18,17 +16,17 @@ const NOME = "Chapolin";
 $idade = 25;
 echo "<p>".NOME." possui $idade anos.</p>";
 ?>
-<p> <b><?=NOME?></b>  possui <i><?=$idade?></i> anos.</p>
-
 <p> 
-    <b class="um"><?=NOME?></b>  possui 
-    <i class="dois"><?=$idade?></i> anos.
-</p>
+    <b class="p-1 text-bg-danger"><?=NOME?></b>  possui 
+    <i class="p-1 text-bg-dark"><?=$idade?></i> anos.
+</p>    
 
-<p> 
-    <span class="um"><?=NOME?></span>  possui 
-    <span class="dois"><?=$idade?></span> anos.
-</p>
+
+</div>
+
+
+
+
 
 
 
