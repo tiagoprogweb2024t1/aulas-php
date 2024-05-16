a80daed364d5e5360ad3949d4941e686d24c14ae
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-16 14:11:01 -0300

    Create 04-condicionais.php

---
+++ 04-condicionais.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Condicionais</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    <h1>Condicionais</h1>
+    <hr>
+
+    <h2>Simples (não tem o <code>else</code> )</h2>
+<?php
+$numero = 50;
+if($numero < 100){
+    /* Este comando só será executado se 
+    a condição for TRUE/VERDADEIRO */
+    echo "<p>Condição é verdadeira/true!</p>";
+}
+?>
+    <hr>
+    <h2>Composta (usa <code>if/else</code>)</h2>
+<?php
+/* Controle de Estoque */
+$produto = "Ultrabook";
+$qtdEmEstoque = 10; // o que temos no momento
+$qtdCritica = 2; // mínimo necessário
+?>
+    <h3>Produto: <?=$produto?></h3>
+    <h3>Estoque: <?=$qtdEmEstoque?></h3>
+
+<?php
+/* Se a quantidade em estoque for abaixo
+da quantidade crítica, o sistema deve avisar
+e pedir pra repor. */
+if(  ){
+
+} else {
+    /* Caso contrário, simplesmente 
+    falar que o estoque está normal. */
+
+}
+?>
+
+
+</div>
+
+</body>
+</html>
