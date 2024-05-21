188dac60ed36731368d7a970ee26bb1e739cf997
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-21 17:10:03 -0300

    Ajustes include

--- site/cabecalho.php
+++ site/cabecalho.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <header>
@@ -12,8 +13,9 @@
             <a href="index.php">Home</a>
             <a href="equipe.php">Equipe</a>
             <a href="portfolio.php">Portfólio</a>
+            <a href="servicos.php">Serviços</a>
         </nav>
     </header>
 
-    <main>
+    <main class="container shadow-lg p-3">
         <article>
---
+++ site/servicos.php
@@ -0,0 +1,9 @@
+<?php include "cabecalho.php"; ?>
+            <h2>Serviços prestados</h2>
+            <ol>
+                <li>Criação</li>
+                <li>Manutenção</li>
+                <li>Divulgação</li>
+            </ol>
+
+<?php include "rodape.php"; ?>
