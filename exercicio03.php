419aa2e8ca54b28ea943a4a0bd1dc662d9709a76
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-21 14:06:22 -0300

    Desafio (apenas com CSS nth-child)

--- exercicio03.php
+++ exercicio03.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 03</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Exercício 03</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+li:nth-child(even) { /* pares */
+    color: red;
+}
+
+li:nth-child(odd){  /* ímpares */
+    color: blue;
+}
+
+</style>
+
 </head>
 <body>
 <div class="container">
