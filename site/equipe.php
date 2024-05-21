b1e225a38a0a3ae34272e5ff085062ed6e159201
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-21 15:22:10 -0300

    Site base

---
+++ site/equipe.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equipe - Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="equipe.php">Equipe</a>
+            <a href="portfolio.php">Portfólio</a>
+        </nav>
+    </header>
+
+    <main>
+        <article>
+            <h2>Nossa equipe</h2>
+            <ul>
+                <li>Tony Stark</li>
+                <li>Chapolin</li>
+                <li>Goku</li>
+            </ul>
+        </article>
+    </main>
+    
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+</html>
---
+++ site/portfolio.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfólio - Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="equipe.php">Equipe</a>
+            <a href="portfolio.php">Portfólio</a>
+        </nav>
+    </header>
+
+    <main>
+        <article>
+            <h2>Nossos trabalhos</h2>
+            <details>
+                <summary>Game</summary>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </details>
+            <details>
+                <summary>Web Site</summary>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, fugiat?</p>
+            </details>
+        </article>
+    </main>
+    
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+</html>
