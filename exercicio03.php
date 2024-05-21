d3780fddd7876d4a24fe38c988a22941f09b5c26
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-21 14:00:14 -0300

    Solução (sem desafio)

---
+++ exercicio03.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 03</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <h1>Exercício 03</h1>
+    <hr>
+<?php
+$mesesDoAno = [
+    'Janeiro','Fevereiro','Março','Abril',
+    'Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'
+];
+?>
+<ol>
+<?php foreach( $mesesDoAno as $mes ){ ?>
+    <li> <?=$mes?> </li>
+<?php } ?>
+</ol>
+
+
+ 
+
+
+</div>
+
+
+
+
+
+
+
+</body>
+</html>
