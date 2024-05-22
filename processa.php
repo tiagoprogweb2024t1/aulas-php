56369beaf7bf848ddcf2280acd0ee11fcfc74ef4
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-22 14:27:21 -0300

    Formulário com PHP

---
+++ processa.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processando os dados</title>
+</head>
+<body>
+    <h1>Processamento dos dados vindos do formulário</h1>
+    <hr>
+
+<?php
+/* Capturando os dados provenientes do formulário
+usando o array superglobal $_POST[]. Se o form usar método get, então usaríamos $_GET[]. */
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$nascimento = $_POST['nascimento'];
+$mensagem = $_POST['mensagem'];
+?>
+    <h2>Dados recebidos</h2>
+    <ul>
+        <li>Nome: <?=$nome?> </li>
+        <li>E-mail: <?=$email?> </li>
+        <li>Data de nascimento: <?=$nascimento?> </li>
+        <li>Mensagem: <?=$mensagem?> </li>
+    </ul>   
+
+</body>
+</html>
