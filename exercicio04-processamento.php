0fcf1d9bec69005ce0c1feef65a8cc42cc041a00
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-22 17:28:00 -0300

    Exercício: solução da etapa 2

--- exercicio04-processamento.php
+++ exercicio04-processamento.php
@@ -12,7 +12,18 @@
 <!-- Faça a partir daqui a programação
 necessária para obter os dados vindos do formulário
 e mostrá-los usando elementos HTML (da forma que você quiser). -->
+<?php
+$produto = $_POST['produto'];
+$fabricante = $_POST['fabricante'];
+$preco = $_POST['preco'];
+$descricao = $_POST['descricao'];
+?>
+    <ul>
+        <li><?=$produto?></li>
+        <li><?=$fabricante?></li>
+        <li>R$ <?=number_format($preco, 2, ",", ".")?></li>
+        <li><?=$descricao?></li>
+    </ul>
 
-    
 </body>
 </html>
